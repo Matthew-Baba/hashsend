0c2692be21527a2cae9bbe2f98383580d3e1bc41
jorniks <dictforum@gmail.com>
2025-03-20 13:22:22 +0100

    Removed unused files and updated dependencies

--- src/lib/network-list.ts
+++
@@ -1,6 +0,0 @@
-import { ChainId } from "@swapsicledex/swapsicle-sdk";
-
-export const NETWORK_LABEL: { [chainId: string]: string } = {
-  [ChainId.TELOS_TESTNET]: 'TELOS Testnet',
-  [ChainId.BSC_TESTNET]: 'BSC Testnet',
-}
--- src/lib/rpc-list.ts
+++
@@ -1,8 +0,0 @@
-import { ChainId } from "@swapsicledex/swapsicle-sdk"
-
-const RPC = {
-  [ChainId.TELOS_TESTNET]: "https://testnet.telos.net/evm",
-  [ChainId.BSC_TESTNET]: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-}
-
-export default RPC
