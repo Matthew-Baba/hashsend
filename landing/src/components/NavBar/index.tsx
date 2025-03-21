8a69cff4063065427b295f405ebdfb7265081e30
jorniks <dictforum@gmail.com>
2025-03-21 09:55:54 +0100

    Testing another theory

--- landing/src/components/NavBar/index.tsx
+++ landing/src/components/NavBar/index.tsx
@@ -9,19 +9,19 @@ const NavBar = () => {
   return (
     <header className="p-4 border-b border-gray-700">
       <div className="flex justify-between items-center app-width">
-        <div className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2">
           <Image src="/img/hashsend.svg" alt="HashSend Logo" width={40} height={40} />
           <span className="font-bold text-xl">HashSend</span>
-        </div>
+        </Link>
 
         <div className="flex items-center gap-8">
-          <a href="#about" className="hover:text-white">
+          <Link href="#about" className="hover:text-white">
             About
-          </a>
+          </Link>
 
-          <a href="#how-it-works" className="hover:text-white">
+          <Link href="#how-it-works" className="hover:text-white">
             How It Works
-          </a>
+          </Link>
         </div>
 
         <div className="flex items-center gap-4">
