7627c922d62386a753b745c626ef8ce3fd0b24d0
jorniks <dictforum@gmail.com>
2025-03-21 10:20:26 +0100

    Testing theory number 7

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -1,11 +1,63 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HashSend() {
 
   return (
     <main className="overflow-y-auto h-full pb-32">
+      <section
+        className="min-h-screen"
+        style={{
+          background: "url('/img/hero-bg.png')",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center'
+        }}
+      >
+        <div className="grid grid-cols-1 sm:grid-cols-9 app-width py-12">
+          <div className="sm:col-span-3 flex flex-col justify-center">
+            <div className="space-y-8">
+              <div className="space-y-6">
+                <h1 className="text-5xl sm:text-7xl font-bold text-yellow-500">
+                  Send Crypto
+                  <br />
+                  Instantly
+                </h1>
+
+                <p className="text-2xl mb-8">No Wallet Address Needed!</p>
+              </div>
+
+              <div className="flex gap-4">
+                <Link href="https://hashsend.vercel.app/" className="btn hs-secondary"> Get Started </Link>
+              </div>
+            </div>
+
+            <Image
+              src="/img/penguin.png"
+              alt="Cartoon penguin holding cryptocurrency coins"
+              width={450}
+              height={450}
+              className=""
+            />
+          </div>
+
+          <div className="sm:col-span-6">
+            <Image
+              src="/img/hero.svg"
+              alt="HashSend hero"
+              width={750}
+              height={650}
+              className="place-self-end"
+            />
+
+            <div className="opacity-30">
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className="min-h-screen">
         <div className="hs-gradient-br py-12 relative overflow-hidden">
           <div className="app-width py-12" id="about">
