4b852213278126a0901e8631e8f3ce6b537ae1a9
jorniks <dictforum@gmail.com>
2025-03-22 18:56:46 +0100

    removed mainnet chain

--- App/src/providers/Web3Provider.tsx
+++ App/src/providers/Web3Provider.tsx
@@ -2,14 +2,14 @@
 
 import { ReactNode } from "react";
 import { WagmiProvider, createConfig } from "wagmi";
-import { mainnet, eduChain, eduChainTestnet } from "wagmi/chains";
+import { eduChainTestnet, eduChain } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
 const config = createConfig(
   getDefaultConfig({
     appName: "HashSend",
-    chains: [mainnet, eduChain, eduChainTestnet],
+    chains: [eduChainTestnet, eduChain],
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || ""
   }),
 );
