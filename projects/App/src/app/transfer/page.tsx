ef3c6822aa6e9b8093695690d0d19cc8a0b69972
jorniks <dictforum@gmail.com>
2025-04-27 12:59:00 +0100

    Updated transfer form and functionality

--- projects/App/src/app/transfer/page.tsx
+++ projects/App/src/app/transfer/page.tsx
@@ -1,41 +1,75 @@
+"use client";
 
 import AppLayout from '@/components/Layout'
+import { useWriteToContract } from '@/hooks/useWriteToContract'
 import { ShieldCheck } from 'lucide-react'
+import { useState } from 'react'
+import { useAccount, useBalance } from 'wagmi';
 
 const Transfers = () => {
+  const { address } = useAccount();
+  const { sendToken } = useWriteToContract()
+  const [amount, setAmount] = useState('')
+  const [recipientAddress, setRecipientAddress] = useState('')
+  const [password, setPassword] = useState('')
+  const { data } = useBalance({ address });
+
   return (
     <AppLayout>
       <main className="app-width max-w-3xl space-y-8">
         <section className="">
           <h4 className="text-4xl font-bold">Transfer</h4>
-          <p className="faded-text font-medium text-sm">Send and receive tokens with ease.</p>
+          <p className="faded-text font-medium text-sm">Send tokens to another anyone across the world. You can also set a password for the transaction.</p>
         </section>
 
         <section className="bg-white p-4 sm:p-8 rounded-lg">
-          <h6 className="font-medium">Send tokens</h6>
+          <h6 className="font-medium text-xl">Send tokens</h6>
 
-          <div className="space-y-6 mt-4">
+          <aside className="space-y-6 mt-4">
             <fieldset className="">
               <span className="font-medium faded-text">Amount</span>
-              <input type="text" className="input-box" placeholder="0.0" />
+              <input type="text" className="input-box" placeholder="0.0" value={amount}
+                onChange={(e) => !isNaN(Number(e.target.value)) && setAmount(e.target.value)}
+              />
+
+              <div className="space-x-1.5 text-sm">
+                <span className="faded-text">Balance:</span>
+                <span className="font-medium">{data?.formatted} {data?.symbol}</span>
+              </div>
             </fieldset>
 
             <fieldset className="">
               <span className="font-medium faded-text">Recipient&apos;s Wallet Address (optional)</span>
-              <input type="text" className="input-box" placeholder="0x...." />
+              <input type="text" className="input-box" placeholder="0x...." value={recipientAddress}
+                onChange={(e) => setRecipientAddress(e.target.value)}
+              />
             </fieldset>
 
             <fieldset className="">
               <span className="font-medium faded-text">Password (optional)</span>
-              <input type="text" className="input-box" placeholder="Secure transaction with password" />
+              <input type="text" className="input-box" placeholder="Secure transaction with password" value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </fieldset>
 
             <fieldset className="pt-4">
-              <button className="btn hs-primary w-full flex items-center place-content-center gap-x-2 py-3">
+              <button className="btn hs-primary w-full flex items-center place-content-center gap-x-2 py-3"
+                onClick={() => { sendToken(amount, recipientAddress, password) }}
+              >
                 <ShieldCheck /> Send Encrypted Transfer
               </button>
             </fieldset>
-          </div>
+          </aside>
+
+          <aside className="mt-6 border-t pt-3 space-y-3">
+            <h6 className="font-medium text-xl">Important Notes</h6>
+
+            <div className="spayce-y-2">
+              <h6 className="faded-text text-sm">Note: The recipient address is optional. If not provided, the transaction will be encrypted with a password.</h6>
+              <h6 className="faded-text text-sm">Please ensure you have enough balance to cover the transaction fee.</h6>
+              <h6 className="faded-text text-sm">You can also use the password to decrypt the transaction later.</h6>
+            </div>
+          </aside>
         </section>
       </main>
     </AppLayout>
